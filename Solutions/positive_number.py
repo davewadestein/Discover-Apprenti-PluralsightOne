f2f59d4feba6ed19829c791f66a301cf054381e9
Dave Wade-Stein <dws@developintelligence.com>
2024-08-12 22:12:55 -0600

    Update positive_number.py

--- Solutions/positive_number.py
+++ Solutions/positive_number.py
@@ -1,8 +1,7 @@
 # get a positive number from the user
 # keep asking until the user complies
-num = 0 # "priming the pump"
+num = int(input("Enter a number greater than 0: "))
 
 while num < 1: # keep going as long as num is NOT >= 1
+    print('Sorry! The number must be >= 0!')
     num = int(input("Enter a positive number: "))
-    if num < 1:
-        print('The number needs to be >= 1')
